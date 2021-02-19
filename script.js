f56feea517c650dda902be27c89904e994048c20
Vinay Chauhan <vinay1011998@gmail.com>
2021-02-19 12:32:57 +0530

    Add script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+console.log(navMenuAnchorTags);
+for(var  i =0;i < navMenuAnchorTags.length;i++)
+{
+    navMenuAnchorTags[i].addEventListener('click',function(event)
+    {
+        event.preventDefault();
+        var targetSectionID = this.textContent.trim().toLowerCase();
+        var targetSection = document.getElementById(targetSectionID);
+        var interval = setInterval(function(){
+            var targetSectionCoordinates = targetSection.getBoundingClientRect();
+            if(targetSectionCoordinates.top <= 0)
+            {
+                clearInterval(interval);
+                return;
+            }
+            window.scrollBy(0,50);
+        },70);
+
+    });
+}
